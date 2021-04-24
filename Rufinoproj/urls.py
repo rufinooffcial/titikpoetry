c8393a5a52d0b9059828e16db3399dfd8b91b124
Rufino <Rufinodelacruz@gsfe.tupcavite.edu.ph>
2021-04-24 22:37:04 +0800

    My first unit passed

--- Rufinoproj/urls.py
+++ Rufinoproj/urls.py
@@ -1,3 +1,11 @@
+from django.conf.urls import url
+from Emergency import views
+
+urlpatterns = [
+	url(r'^$', views.MainPage, name='MainPage'),
+]
+
+
 """Rufinoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +21,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+"""
+
