321764925763d91289ebaa57c9914848434a904f
Rufino <Rufinodelacruz@gsfe.tupcavite.edu.ph>
2021-04-24 21:41:56 +0800

    New FunctionalTest Module

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,3 +1,19 @@
 from selenium import webdriver 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+import unittest
+
+class Pagetesting(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_browser_title(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('Rufinoproj', self.browser.title)
+		self.fail('Finish the test NOW')
+
+
+if __name__ == '__main__':
+	unittest.main()
