be5964535e81591fc40189c5d95f60975e41a7dc
Rufino <Rufinodelacruz@gsfe.tupcavite.edu.ph>
2021-04-25 00:10:24 +0800

    still my first functional test

--- Emergency/views.py
+++ Emergency/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def MainPage(request):
-	return HttpResponse('<html><title>Call an Emergency</title><h1 style="color:red">Call for Emergency</h1><input type="text" id="newEntry" name="Entry1" placeholder="Send your location!&#063">&nbsp<input type="submit" value="Confirm"></html>')
+	return HttpResponse('<html><title>Call an Emergency</title><h1 style="color:red">Call for Emergency now</h1><input type="text" id="newEntry" name="Entry1" placeholder="Send your location!&#063">&nbsp<input type="submit" value="Confirm"></html>')
