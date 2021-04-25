35384400bc7b246c887bdcd504b3f9d841b47be2
Rufino <Rufinodelacruz@gsfe.tupcavite.edu.ph>
2021-04-25 20:56:18 +0800

    finally shorter test.py

--- Emergency/tests.py
+++ Emergency/tests.py
@@ -1,18 +1,43 @@
-from django.urls import resolve
 from django.test import TestCase
-from Emergency.views import MainPage
-from django.http import HttpRequest 
+
+
+
 	
 class HomePageTest(TestCase):
-	
-	def test_root_url_resolves_to_mainpage_view(self):
+
+	def test_mainpage_returns_correct_view(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'mainpage.html')
+
+
+
+	'''
+		from django.urls import resolve
+		from django.http import HttpRequest 
+		from django.template.loader import render_to_string
+		from Emergency.views import MainPage
+
+		def test_mainpage_returns_correct_view(self):
+		response = self.client.get('/')
+		html = response.content.decode('utf8')
+		string_html = render_to_string('mainpage.html')
+		self.assertEqual(html, string_html)
+		self.assertTemplateUsed(response, 'mainpage.html')
+
+		def test_root_url_resolves_to_mainpage_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, MainPage)
+		def test_mainpage_returns_correct_view(self):
+		request = HttpRequest()
+		response = MainPage(request)
+		html = response.content.decode('utf8')
+		string_html = render_to_string('mainpage.html')
+		self.assertEqual(html, string_html)'''
 
-def test_mainpage_returns_correct_view(self):
-	request = HttpRequest()
-	response = MainPage(request)
-	html = response.content.decode('utf8')
-	self.assertTrue(html.startswitch('<html>'))
-	self.assertIn('<title>Call an Emergency</title>', html)
-	self.assertTrue(html.endswith('</html>'))
+	'''def test_mainpage_returns_correct_view(self):
+		request = HttpRequest()
+		response = MainPage(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.strip().startswitch('<html>'))
+		self.assertIn('<title>Book Now in PPPC</title>', html)
+		self.assertTrue(html.endswith('</html>'))'''
--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,39 +1,51 @@
 from selenium import webdriver 
 import unittest
 
-from selenium.webdriver common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 import time
 
+class PageTesting(unittest.TestCase):
 
-
-
-class Pagetesting(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-
-	def tearDown(self):
-		self.browser.quit()
+	
 
 	def test_browser_title(self):
 		self.browser.get('http://localhost:8000')
-		self.assertIn('Call an Emergency', self.browser.title)
-		#self.fail('Finish the test NOW')
+		self.assertIn('Titik Poetry Inc.', self.browser.title)
 
-	def test_start_list_and_retrieve_it(self):
-		self.browser.get('http://localhost:8000')
-		self.assertIn('Call for Emergency', self.browser.title)
-		headerText = self.browser.find_element_by_tag_name('h1').headerText
-		self.assertIn('Emergency SOS', headerText)
-		inputbox = self.browser.find_element_by_id('idNewEntry')
-		self.assertEqual(inputbox.get_attribute('placeholder'),'Person name')
-		inputbox.send_keys('TUP Cavite')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		table = self.browser.find_element_by_id('idListTable')
-		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(any(row.text =='1: TUP Cavite'))
-		self.fail('Finish test')
+
+		hText = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Audition Form', hText)
+
+		
+		paragraph = self.browser.find_element_by_tag_name('p').text
+		self.assertIn('Personal Info', paragraph)
+		
+		
+		label = self.browser.find_element_by_id('name1').text
+		self.assertIn('Name:', label)
+		Name = self.browser.find_element_by_name('name').send_keys("Rufino Araza Dela Cruz")
+		time.sleep(.5)
+		label1 = self.browser.find_element_by_id('date').text
+		self.assertIn('Birthday', label1)
+		Location = self.browser.find_element_by_name('bday').send_keys("Nov 11, 1998")
+		time.sleep(.5)
+		label2 = self.browser.find_element_by_id('mail').text
+		self.assertIn('Email', label2)
+		lastname = self.browser.find_element_by_name('email').send_keys("rufino.delacruz@gsfe.com")
+		time.sleep(.5)
+		label3 = self.browser.find_element_by_id('sample').text
+		self.assertIn('Sample of your Piece', label3)
+		lastname = self.browser.find_element_by_name('sample').send_keys("akoy isang makata")
+		time.sleep(2.5)
+		submitbutton = self.browser.find_element_by_id('clear').click()
+
+
+
+		
+
 
 if __name__ == '__main__':
 	unittest.main()
